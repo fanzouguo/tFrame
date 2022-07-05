1182010203d1be08d073b6bc6182d819cb8f6c60
上海深普软件.Co.,Ltd <leev@smpoo.com>
2022-07-06 00:09:25 +0800

    自动提交测试

--- .dev/build.js
+++ .dev/build.js
@@ -1,10 +1,14 @@
 import { cmd } from './sysCmd.js';
 
 const exec = async () => {
-	console.clear();
-	await cmd('git add .');
-	await cmd('git commit -m "自动提交测试"');
-	await cmd('git push -u origin main');
+	try {
+		console.clear();
+		await cmd('git add .');
+		await cmd('git commit -m "自动提交测试"');
+		await cmd('git push -u origin main');
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 exec();
